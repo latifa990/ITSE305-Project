f45d9d8edc750212f93dacc60d6b075c6c27e38b
latifa990 <latifaymzubari@gmail.com>
2026-05-16 16:08:16 +0300

    Applied review changes by rawan
    Added a check for borrow limit and updated borrow count.

--- src/Latifa/BBbusiness.java
+++ src/Latifa/BBbusiness.java
@@ -56,6 +56,10 @@ public class BBbusiness {
             String name = dataLayer.getMemberName(memberId);
             return name + " has reached the borrow limit of " + borrowLimit + " books.";
         }
+        
+        // all checks passed so update the data
+        dataLayer.decreaseCopy(bookId);
+        dataLayer.increaseBorrowCount(memberId);
 
         // retrieve details inline and return success message
         return "The book " + dataLayer.getBookTitle(bookId) +
